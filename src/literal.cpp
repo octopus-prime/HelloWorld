11587c70e94b60b74e0242a5cbdd3bd2a6c5323a
octopus-prime <mike.gresens@gmail.com>
2023-03-08 19:06:17 +0100

    Windows

--- src/literal.cpp
+++ src/literal.cpp
@@ -1,6 +1,8 @@
 #include "literal.hpp"
 
-static_assert("a1b3"_b == 131073, "board a1b3");
+static_assert("a"_f == 72340172838076673ull, "file a");
+static_assert("1"_r == 255ull, "rank 1");
+static_assert("a1b3"_b == 131073ull, "board a1b3");
 static_assert("a1"_s == 0, "square a1");
 static_assert("b3"_s == 17, "square b3");
 static_assert("h8"_s == 63, "square h8");
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 
 int main() {
   std::cout << "Hello world" << std::endl;
+  std::cout << "a"_f << std::endl;
+  std::cout << "1"_r << std::endl;
   std::cout << "a1b3"_b << std::endl;
   std::cout << "a1"_s << std::endl;
   std::cout << "b3"_s << std::endl;
